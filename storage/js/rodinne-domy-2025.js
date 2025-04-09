58fdbc1e2c88e7ee6772f71f3e4dfaca4787e3a5
Tom Vitha <tomasvitha01@gmail.com>
2025-04-09 14:02:38 +0200

    Mid Day 2

--- storage/js/rodinne-domy-2025.js
+++ storage/js/rodinne-domy-2025.js
@@ -1,1 +1,17 @@
-console.log('test')
+$(document).ready(function () {
+
+    //* Expand button for layout viewer
+    $('[data-lv-expand]').on('click', function (event) {
+        console.log($(this))
+        event.preventDefault();                                             // Prevent default action for <a> elements
+        event.stopPropagation();                                            // Prevent event from bubbling up
+        const parentLV = $(this).parents('.layout-viewer')[0]               // Get the closest
+        console.log(parentLV)                          // Log the parent layout viewer element
+        $(parentLV).toggleClass('expanded')                 // Toggle the expanded class on the layout viewer
+        $(this).children('[class*="fa-"]').toggleClass('fa-expand fa-compress');
+    })
+
+    
+})
+
+console.log('sdgsfg');
