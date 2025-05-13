c17cec6dda0e07a4a18caf8de33d410d85c2fbc8
Tom Vitha <tomasvitha01@gmail.com>
2025-05-13 10:43:24 +0200

    Implement MVP Price table

--- storage/js/rodinne-domy-2025.js
+++ storage/js/rodinne-domy-2025.js
@@ -1,3 +1,37 @@
+// ?  FANCYBOX  ? //
+try {
+    // Initialization
+    Fancybox.bind('[data-fancybox]', {
+        Carousel: {
+            transition: "slide",
+        },
+        dragToClose: false,
+        l10n: Fancybox.l10n.cs,     // Czech language for UI
+        Thumbs: {
+            showOnStart: false,
+            type: "classic",
+        },
+        Toolbar: {
+            display: {
+                left: [
+                    "thumbs",
+                    "infobar",
+                ],
+                middle: [
+                    "zoomIn",
+                    "zoomOut",
+                ],
+                right: [
+                    "close"
+                ],
+            },
+        },
+    });
+} catch (error) {
+    console.warn('Fancybox failed', error);
+}
+
+
 $(document).ready(function () {
 
     //* Expand button for layout viewer
